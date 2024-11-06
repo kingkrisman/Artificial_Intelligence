c012545a3844c544a4d6f4f61cdcd2b34f965dc6
kingkrisman <kingkrisman@gmail.com>
2024-11-06 03:24:26 -1100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/sidebar/sidebar.jsx'
+import Sidebar from './components/sidebar/Sidebar.jsx'
 import Main from './components/main/Main.jsx'
 
 
